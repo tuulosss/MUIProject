16050d66e6d4bcd207d213d8bf12fd4ae49cb14f
Iiro Pääkkönen <45567925+iiropaakkonen@users.noreply.github.com>
2025-03-13 13:50:41 +0200

    Updates to find_brightest

--- find_brightest.py
+++ find_brightest.py
@@ -1,14 +1,20 @@
 import cv2
 
 def find_brightest(frame):
-    #Findin the brightest spot in "frame"
 
+    #Finding the brightest spot in "frame"
     #turn to grayscale
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    object_detect = cv2.createBackgroundSubtractorMOG2()
+    mask = object_detect.apply(frame)
 
     # perform a naive attempt to find the (x, y) coordinates of
     # the area of the image with the largest intensity value
     blur = cv2.blur(gray, (20,20))
     (minVal, maxVal, minLoc, maxLoc) = cv2.minMaxLoc(blur)
-    cv2.circle(frame, maxLoc, 5, (255, 0, 0), 2)
-    point = maxLoc
+    
+    #HIGHEST VALUE OF THE IMAGE. BY ADJUSTING, CHANGES THE THRESHOLD OF THE FLASHLIGHT VALUE
+    if maxVal > 225:
+        cv2.circle(frame, maxLoc, 5, (100, 255, 0), 2)
+        point = maxLoc
